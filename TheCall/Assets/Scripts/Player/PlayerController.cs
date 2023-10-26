bfc4a45b1b062e87bc49b0b0dcc8a3537c286e01
AIE\s213209 <emmanuel.letton@gmail.com>
2023-10-26 15:16:03 +1030

    Created & Added project files for the Unity project using the HDRP. Added folder structure for assets Created temporary character controller. - Emmanuel Williams - 26/10/2023 3:15pm

---
+++ TheCall/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController controller;
+    [SerializeField]
+    private float mouseSense = 200;
+    [SerializeField]
+    private float speed = 100;
+    private float m_mouseX = 0;
+    private float m_mouseY = 0;
+    private float m_inputX = 0;
+    private float m_inputZ = 0;
+    private Vector3 m_velocity;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_mouseX += Input.GetAxis("Mouse X") * mouseSense * Time.deltaTime;
+        m_mouseY += Input.GetAxis("Mouse Y") * mouseSense * Time.deltaTime;
+
+        transform.rotation = Quaternion.Euler(new Vector3(0, m_mouseX, 0));
+        transform.rotation *= Quaternion.Euler(new Vector3(m_mouseY, 0, 0));
+
+        m_inputX = Input.GetAxis("Horizontal");
+        m_inputZ = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * -m_inputX + transform.forward * -m_inputZ;
+        move.y = 0;
+
+        controller.Move(move * speed * Time.deltaTime);
+        m_velocity.y++;
+        //transform.position -= move * speed * Time.deltaTime;
+    }
+}
