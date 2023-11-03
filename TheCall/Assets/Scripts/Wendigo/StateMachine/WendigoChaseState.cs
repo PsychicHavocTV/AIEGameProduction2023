84fe291d8a03f2632ba8bb0a26c9e15a23f92f3f
AIE\s213209 <emmanuel.letton@gmail.com>
2023-11-03 16:39:19 +1030

    Completed main navmesh and pathfinding testing. overhauled the chasing system for the Wendigo so they can detect the player properly

--- TheCall/Assets/Scripts/Wendigo/StateMachine/WendigoChaseState.cs
+++ TheCall/Assets/Scripts/Wendigo/StateMachine/WendigoChaseState.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class WendigoChaseState : MonoBehaviour
+public class WendigoChaseState : BaseState
 {
-    // Start is called before the first frame update
-    void Start()
+    public NavMeshAgent nma;
+    float playerDistance;
+
+    public override void EnterState(WendigoStateManager wendigo)
     {
-        
+        nma.speed = 9.5f;
+        Debug.Log("CHASING!!");
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void UpdateState(WendigoStateManager wendigo)
     {
-        
+        RaycastHit hit;
+        playerDistance = Vector3.Distance(wendigo.playerRef.transform.position, wendigo.transform.position);
+
+        //if (Physics.Raycast(wendigo.RaycastOrigin.transform.position, wendigo.RaycastOrigin.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        //{
+            //Debug.Log(hit.transform.tag);
+            //if (hit.transform.gameObject.tag == "Player")
+            //{
+                nma.ResetPath();
+                nma.SetDestination(wendigo.playerRef.transform.position);
+        //}
+        //}
+        Vector3 rayDirection = wendigo.playerRef.transform.position - wendigo.transform.position;
+         if ((Vector3.Angle(rayDirection, wendigo.transform.forward)) < 25) 
+         {
+             if ((Vector3.Angle(rayDirection, wendigo.transform.forward)) < 25) // Is player within field of view
+             {
+                if (Physics.Raycast(wendigo.transform.position, rayDirection, out hit, 30))
+                {
+                    if (hit.collider.gameObject.tag == "Player")
+                    {
+                        //Debug.Log("player in front");
+                        wendigo.StartChasing();
+                    }
+                    else
+                    {
+                        Debug.Log(playerDistance);
+                        if (wendigo.timerRunning == false)
+                        {
+                            wendigo.ControlTimer();
+                        }
+                    }
+                }
+                else
+                {
+                    Debug.Log(playerDistance);
+                    if (wendigo.timerRunning == false)
+                    {
+                        wendigo.ControlTimer();
+                    }
+                }
+             }
+         }
     }
 }
