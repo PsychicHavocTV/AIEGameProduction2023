9cafca7fc55149df5ec305fcf5eed711736765d2
AIE\s210991 <s210991@students.aie.edu.au>
2023-11-09 09:55:25 +1030

    Added additional information to the key object descriptor.

--- TheCall/Assets/Scripts/Player/Photography/KeyObjectDescriptor.cs
+++ TheCall/Assets/Scripts/Player/Photography/KeyObjectDescriptor.cs
@@ -6,4 +6,13 @@ public class KeyObjectDescriptor : MonoBehaviour
     [Tooltip("The name of the object.")]
     public string objectName;
 
+    [Tooltip("The description of the object.")]
+    public string objectDescription;
+
+    [Tooltip("The value of the object.")]
+    public int objectValue;
+
+    [Tooltip("The threshold for how much of the object needs to be taken up on the screen for it to be recognized.")]
+    public float objectThreshold;
+
 }
